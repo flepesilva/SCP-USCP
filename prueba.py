f935de4eb45bee0893e0edc9725f83c6a405cbf0
FelipeCisternasCaneo <felipe.cisternas@pucv.cl>
2024-09-22 23:26:18 -0300

    optimizacion de tiempos

--- prueba.py
+++ prueba.py
@@ -1,41 +1,40 @@
-# contador = 1
-# while contador <= 5:
-#     print("Iteración: ",contador)
-#     contador += 1
-    
-# suma = 0
-# while suma < 100:
-#     numero = int(input("Introduce un número: "))
-#     suma += numero
-#     print("Suma actual: ",suma)
-    
-    
-    
-# continuar = True
-# while continuar:
-#     numero = int(input("Introduce un número (0 para terminar): "))
-#     if numero == 0:
-#         continuar = False
-#     else:
-#         print("Número introducido:", numero)
+import numpy as np
+import pandas as pd
+import os
+import time
+from Problem.SCP.problem import SCP
 
-
-a = 943
-continuar = True
-n = 0
-b = 0
-c = 0
-while continuar:
-    d = a % 10
-    a = a // 10
-    n = n * 10 + d	
-    if d % 2 == 0:
-        b+=1
-    else:
-        c+=1
-    if a == 0:
-        continuar = False
-print(n)
-print(b)
-print(c)
-
+instancia = 'scp41'
+for instance in instancias:
+    instancia = SCP(instance.split('.')[0])
+    block_sizes = []
+    i = 1
+    df = pd.DataFrame(columns=['id','best block size', 'best time', 'worst block size', 'worst time', 'total time'])
+    while i <= (instancia.getColumns()/10):
+        block_sizes.append(i*10)
+        i+=1
+    for i in range(31):
+        best_time = float('inf')
+        best_block_size = None
+        worst_time = 0
+        worst_block_size = None
+        
+        suma = 0
+        for block_size in block_sizes:
+            start_time = time.time()
+            solution = np.zeros(instancia.getColumns())
+            flag, aux = instancia.factibilityTest(solution, block_size)
+            if not flag: #solucion infactible
+                solution = instancia.repair(solution, 'complex', block_size)
+            fitness = instancia.fitness(solution, block_size)
+            elapsed_time = time.time() - start_time
+            if elapsed_time < best_time:
+                best_time = elapsed_time
+                best_block_size = block_size
+            if elapsed_time > worst_time:
+                worst_time = elapsed_time
+                worst_block_size = block_size
+            suma+=elapsed_time
+        print(f"{instance.split('.')[0]}, BS: {best_block_size}, BT: {best_time:.6f}, WS: {worst_block_size}, WT: {worst_time:.6f}, TT: {suma:.6f}, total exp: {len(block_sizes)}, DF: {(((worst_time*100)/best_time)-100):.2f} %")
+        df.loc[i] = [i, best_block_size, best_time, worst_block_size, worst_time, suma]
+    df.to_csv(f'./Problem/USCP/Analisis/{instance.split(".")[0]}.csv', index=False)
